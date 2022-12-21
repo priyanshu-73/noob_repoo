5acf1fdfe48b43d8ab2bb961caa6e5eeb4844e83
priyanshu-73 <112119073+priyanshu-73@users.noreply.github.com>
2022-12-21 22:44:35 +0530

    Add files via upload

---
+++ aa1_set3.py
@@ -0,0 +1,18 @@
+#lex_auth_012693797166096384149
+
+def find_leap_years(given_year):
+
+    # Write your logic here
+    list_of_leap_years=[]
+
+    while len(list_of_leap_years) < 15:
+        if given_year % 4 == 0 :
+            if given_year % 4 == 0 and given_year % 100 != 0:
+                list_of_leap_years.append(given_year)
+        given_year += 1
+
+
+    return list_of_leap_years
+
+list_of_leap_years=find_leap_years(1580)
+print(list_of_leap_years)
